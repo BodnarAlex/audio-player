8093e4f6e479fda168175a68730eb7f10033a3fc
Bodnar Alex <allugovskova@mail.ru>
2023-09-21 23:36:41 +0200

    feat: add script for play and pause

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 const songs = [
-    {autor: "Beyonce", title: "Don't Hurt Yourself", url: "url(assets/img/lemonade.png)"},
-    {autor: "Dua Lipa", title: "Don't Start Now", url: "url(assets/img/dontstartnow.png)"},
+    {autor: "Beyonce", title: "Don't Hurt Yourself", url: "url(assets/img/lemonade.png)", song:'assets/audio/beyonce.mp3'},
+    {autor: "Dua Lipa", title: "Don't Start Now", url: "url(assets/img/dontstartnow.png)", song:'assets/audio/assets_audio_dontstartnow.mp3'},
 ];
 
 let counter = 0;
+const audio = new Audio();
 
 function next() {
     counter = (counter + 1) % songs.length;
@@ -14,9 +15,26 @@ function before() {
     counter = (counter - 1) > -1 ? counter - 1 : songs.length-1;
     newSong(counter);
 }
+
 function newSong(count){
+    audio.src = songs[counter].song;
+    console.log(audio);
     document.getElementById("title").textContent = songs[count].title;
     document.getElementById("autor").textContent = songs[count].autor;
     document.body.style.backgroundImage = songs[count].url;
     document.getElementById("image_albom").style.backgroundImage = songs[count].url;
+}
+
+
+function play(){
+    audio.play();
+    document.getElementsByClassName("play")[0].style.display = 'none';
+    document.getElementsByClassName("stop")[0].style.display = 'block';
+}
+
+
+function stop(){
+    audio.pause();
+    document.getElementsByClassName("stop")[0].style.display = 'none';
+    document.getElementsByClassName("play")[0].style.display = 'block';
 }
