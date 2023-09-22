7ede43ed8aaaee9e9c232cf74b2d16d5aaa567bf
Bodnar Alex <allugovskova@mail.ru>
2023-09-22 23:50:30 +0200

    refactor: improve code visualization

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const songs = [
-    {autor: "Beyonce", title: "Don't Hurt Yourself", url: "url(assets/img/lemonade.png)", song:'assets/audio/beyonce.mp3'},
-    {autor: "Dua Lipa", title: "Don't Start Now", url: "url(assets/img/dontstartnow.png)", song:'assets/audio/assets_audio_dontstartnow.mp3'},
+    { autor: "Beyonce", title: "Don't Hurt Yourself", url: "url(assets/img/lemonade.png)", song: 'assets/audio/beyonce.mp3' },
+    { autor: "Dua Lipa", title: "Don't Start Now", url: "url(assets/img/dontstartnow.png)", song: 'assets/audio/assets_audio_dontstartnow.mp3' },
 ];
 
 let counter = 0;
@@ -8,15 +8,15 @@ let correctTimer = 0;
 const audio = new Audio();
 audio.src = songs[counter].song;
 
-audio.addEventListener('timeupdate',function (){
+audio.addEventListener('timeupdate', function () {
     document.getElementById("start_song").textContent = getCorrectTimeformat(audio.currentTime);
     if (audio.currentTime == 0)
-        document.getElementById("progress").value = 0 ;
+        document.getElementById("progress").value = 0;
     else
         document.getElementById("progress").value = audio.currentTime * 500 / audio.duration;
 });
 
-audio.onloadeddata = function(){
+audio.onloadeddata = function () {
     let duration = getCorrectTimeformat(audio.duration);
     document.getElementById("end_song").textContent = duration;
 }
@@ -27,42 +27,43 @@ function next() {
 }
 
 function before() {
-    counter = (counter - 1) > -1 ? counter - 1 : songs.length-1;
+    counter = (counter - 1) > -1 ? counter - 1 : songs.length - 1;
     newSong(counter);
 }
 
-function newSong(count){
+function newSong(count) {
     audio.src = songs[counter].song;
     document.getElementById("title").textContent = songs[count].title;
     document.getElementById("autor").textContent = songs[count].autor;
-    document.body.style.backgroundImage = songs[count].url;
     document.getElementById("image_albom").style.backgroundImage = songs[count].url;
-    document.getElementsByClassName("stop")[0].style.display = 'none';
-    document.getElementsByClassName("play")[0].style.display = 'block';
+    document.body.style.backgroundImage = songs[count].url;
+    toggleDifferentClass("play", "stop");
 }
 
-function play(){
+function play() {
     audio.play();
-    document.getElementsByClassName("play")[0].style.display = 'none';
-    document.getElementsByClassName("stop")[0].style.display = 'block';
+    toggleDifferentClass("stop", "play");
 }
 
-function stop(){
-    correctTimer = audio.currentTime;
+function stop() {
     audio.pause();
-    document.getElementsByClassName("stop")[0].style.display = 'none';
-    document.getElementsByClassName("play")[0].style.display = 'block';
+    toggleDifferentClass("play", "stop");
 }
 
-function otherTime(){
+function otherTime() {
     let needTimeInterest = document.getElementById("progress").value;
     let needTime = getCorrectTimeformat(needTimeInterest * audio.duration / 500);
     document.getElementById("start_song").textContent = needTime;
     audio.currentTime = needTimeInterest * audio.duration / 500;
 }
 
-function getCorrectTimeformat(sec){
-    let secs = Math.floor(sec % 60); 
-    secs = secs > 9 ? secs : '0'+ secs;
-    return Math.trunc(sec / 60) + ':' +  secs;
+function getCorrectTimeformat(sec) {
+    let secs = Math.floor(sec % 60);
+    secs = secs > 9 ? secs : '0' + secs;
+    return Math.trunc(sec / 60) + ':' + secs;
+}
+
+function toggleDifferentClass(visiable, none) {
+    document.getElementsByClassName(none)[0].style.display = 'none';
+    document.getElementsByClassName(visiable)[0].style.display = 'block';
 }
